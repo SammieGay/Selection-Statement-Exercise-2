567c51fec204f1b17039bc18cf415c9642fd3cec
Sammie Gay <smylezstudioz@gmail.com>
2023-09-22 17:33:45 -0500

    Finish Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Whats your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I love math!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("I enjoy the writting part of english.");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science labs are fun, but i would sometimes get lost.");
+                    break;
+
+                case "social studies":
+                    Console.WriteLine("I was not a fan of socia studies, it just bored me!");
+                    break;
+
+                case "physical education":
+                    Console.WriteLine("Yes!! P.E. was where we had all the fun!");
+                    break;
+
+                default:
+                    Console.WriteLine("I dont know of that subject.");
+                    break;
+            }
         }
     }
 }
